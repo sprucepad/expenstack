970c5b49e37b4c392a0e160bec9f4791ada8e932
sprucepad <sprucepadhq@gmail.com>
2026-07-06 12:46:20 -0300

    feat: set up i18n

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,8 @@
 import "@/global.css";
+import "@/i18n";
 import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { useTranslation } from "react-i18next";
 import { useColorScheme } from "react-native";
 import "temporal-polyfill/global";
 
@@ -8,11 +10,17 @@ SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+
+  const { t } = useTranslation();
+
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="[dateString]" options={{ title: "Expenses" }} />
+        <Stack.Screen
+          name="[dateString]"
+          options={{ title: t("main.title") }}
+        />
       </Stack>
     </ThemeProvider>
   );
---
+++ src/i18n.ts
@@ -0,0 +1,33 @@
+import * as Localization from "expo-localization";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "./locales/en.json";
+import pt from "./locales/pt.json";
+
+const defaultLocale = Localization.getLocales()[0].languageCode ?? "en";
+
+// this is how i18next works
+// eslint-disable-next-line import/no-named-as-default-member
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    pt: { translation: pt },
+  },
+  lng: defaultLocale,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false, // React already escapes
+  },
+});
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    defaultNS: "translation";
+    resources: {
+      translation: typeof en;
+    };
+  }
+}
+
+export default i18n;
